cd21661b545f9a1ce6908887ffaa6114ee1d1af4
sandhyareddy451 <vemula.sandhya51@gmail.com>
2025-08-06 15:17:14 +0530

    mongo db connection

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    
+    name = "us_visa",
+    version = "0.0.0",
+    author = "sandhya",
+    author_email  = "vemula.sandhya51@gmail.com",
+    packages = find_packages()
+    
+    
+    
+)
